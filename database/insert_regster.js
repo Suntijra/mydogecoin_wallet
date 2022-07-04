e7ed55d7f65da1bf9b1017f9341eaec9f8ac6065
Sunti <52416033+Suntijra@users.noreply.github.com>
2022-07-04 21:11:21 +0700

    เปลียน เป็น server ของ unit

--- database/insert_regster.js
+++ database/insert_regster.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://santi:Santi!12321@157.245.59.56:27018/?authSource=admin&readPreference=primary&directConnection=true&ssl=false";
-
+// var url = "mongodb://santi:Santi!12321@157.245.59.56:27018/?authSource=admin&readPreference=primary&directConnection=true&ssl=false";
+var url = "mongodb://dom:dom123@167.99.71.116:27017/?directConnection=true&appName=mongosh+1.5.0&authMechanism=DEFAULT";
 function Registerdb(user,pass,S_reset =false,S_login =true){
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const MD5 = require('js-md5');
 
 const port = 8000
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://santi:Santi!12321@157.245.59.56:27018/?authSource=admin&readPreference=primary&directConnection=true&ssl=false";
+// server Unit
+var url = "mongodb://dom:dom123@167.99.71.116:27017/?directConnection=true&appName=mongosh+1.5.0&authMechanism=DEFAULT";
 
 
 
