4ec2ac9b7fb6d776c47ef22148685a25d2c475c1
Sunti <saknsaza87@gmail.com>
2022-07-26 12:15:21 +0700

    update

---
+++ Mobile/js/listaccount.js
@@ -0,0 +1,16 @@
+axios.post("http://167.99.71.116:3000/api/listaddress", {
+    "token": localStorage.getItem("token")
+}).then((result) => {
+    let listaccouunt = result.data.listaddr
+    let innerhtml;
+    listaccouunt.forEach(element => {
+        console.log(element)
+        innerhtml += ` <div class="scroll-inside-contant">
+        <img style="height: 38px;" src="./img/profilemoon.png" alt="">
+        <span class="accgen">${element}</span>
+    </div>`
+    })
+    document.getElementById("account_list").innerHTML = innerhtml
+}).catch((err) => {
+    console.log(err)
+});
